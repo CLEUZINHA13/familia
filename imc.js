1bebeb91824c7254f5ac11dcf49307ff01faca95
Rayssa <141257109+CLEUZINHA13@users.noreply.github.com>
2024-11-08 11:12:06 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,6 @@
+let peso = parseFloat(prompt("informe o seu peso:"));
+let altura = parseFloat(prompt("informe a sua altura:"));
+
+if(isNaN(peso) || ispn(altura)|peso<=0||altura){
+    document.write("porfavor informar valores valitos")
+}
